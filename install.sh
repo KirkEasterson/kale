faaef56332c6e096360eb307097dc6041bda32bb
kirkeasterson <kirk.easterson@gmail.com>
2021-12-31 00:11:03 +0100

    First list of files

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+programs=(
+		"git"
+		"vim"
+		"curl"
+		"tree"
+		"fonts-ubuntu-font-family-console"
+		"rxvt-unicode-256color"
+		"pop-gtk-theme"
+		"pop-icon-theme"
+		"compton"
+		"xsel"
+		"ranger"
+		"firefox"
+		"feh"
+		"lxappearance"
+		"libnotify-bin"
+		"alsa-base"
+		"alsamixergui"
+		"volumeicon-alsa"
+		"network-manager-gnome"
+		"tmux"
+		"neovim"
+		"xinit"
+		"xorg"
+		"i3"
+)
+
+echo ${programs[@]}
