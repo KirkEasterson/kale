8a5ff2d9396d7a6d48836714bab509de48bbbab2
kirkeasterson <kirk.easterson@gmail.com>
2021-12-31 16:01:15 +0100

    Added snap programs

--- program-install.sh
+++ program-install.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
 
-programs=(
-		"git"
-		"vim"
-		"curl"
-		"tree"
-		"fonts-ubuntu-font-family-console"
-		"rxvt-unicode-256color"
-		"pop-gtk-theme"
-		"pop-icon-theme"
-		"compton"
-		"xsel"
-		"ranger"
-		"firefox"
-		"feh"
-		"lxappearance"
-		"libnotify-bin"
-		"alsa-base"
-		"alsamixergui"
-		"volumeicon-alsa"
-		"network-manager-gnome"
-		"tmux"
-		"neovim"
-		"xinit"
-		"xorg"
-		"i3"
+
+# TODO: Figure out which programs are not needed
+apt_prgms=(
+	"git"
+	"vim"
+	"curl"
+	"tree"
+	"fonts-ubuntu-font-family-console"
+	"rxvt-unicode-256color"
+	"pop-gtk-theme"
+	"pop-icon-theme"
+	"compton"
+	"xsel"
+	"ranger"
+	"firefox"
+	"feh"
+	"lxappearance"
+	"libnotify-bin"
+	"alsa-base"
+	"alsamixergui"
+	"volumeicon-alsa"
+	"network-manager-gnome"
+	"tmux"
+	"neovim"
+	"xinit"
+	"xorg"
+	"i3"
 )
 
-sudo apt install -y ${programs[@]}
+snap_prgrms=(
+	"code -- classic"
+	"chromium"
+	"1password"
+	"postman"
+	"whatsdesk"
+	"caprine"
+)
+
+sudo apt install -y ${apt_prgms[@]}
+sudo apt autoremove
+
+for entry in "${snap_prgrms[@]}"
+do
+	sudo snap install $entry
+done
