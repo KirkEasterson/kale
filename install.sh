5c62e19f43919fbd1382c5b725d44a84b2d472b8
Kirk Easterson <kirk.easterson@gmail.com>
2024-04-07 12:53:31 +0200

    feat(bootstrap): update install script

--- install.sh
+++ install.sh
@@ -3,35 +3,34 @@
 # TODO: personalize this
 set -e
 
-DOTFILES_DIR="$HOME/.dotfiles"
+KALE_DIR="$HOME/kale"
 SSH_DIR="$HOME/.ssh"
 
 if ! [ -x "$(command -v ansible)" ]; then
-	sudo pacman -S ansible --noconfirm
+	echo "ansible must be installed"
+	exit 1
 fi
 
 if ! [[ -f "$SSH_DIR/id_rsa" ]]; then
 	mkdir -p "$SSH_DIR"
-
 	chmod 700 "$SSH_DIR"
 
 	ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "$USER@$HOSTNAME"
-
 	cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
 
 	chmod 600 "$SSH_DIR/authorized_keys"
 fi
 
 
-if [[ -f "$DOTFILES_DIR/requirements.yml" ]]; then
-	cd "$DOTFILES_DIR"
+if [[ -f "$KALE_DIR/requirements.yml" ]]; then
+	cd "$KALE_DIR"
 	ansible-galaxy install -r requirements.yml
 fi
 
-cd "$DOTFILES_DIR"
+cd "$KALE_DIR"
 
-if [[ -f "$DOTFILES_DIR/vault-password.txt" ]]; then
-	ansible-playbook --diff --vault-password-file "$DOTFILES_DIR/vault-password.txt" "$DOTFILES_DIR/main.yml"
+if [[ -f "$KALE_DIR/vault-password.txt" ]]; then
+	ansible-playbook --diff --vault-password-file "$KALE_DIR/vault-password.txt" "$KALE_DIR/bootstrap.yml"
 else
-	ansible-playbook --diff "$DOTFILES_DIR/main.yml"
+	ansible-playbook --diff -ask-vault-pass "$KALE_DIR/bootstrap.yml"
 fi
