32d4051de58c457b1ac957186d493db84bb359bb
Kirk Easterson <kirk.easterson@gmail.com>
2024-04-14 21:54:40 +0200

    feat(install): re-add sshkey gen to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# TODO: make this script POSIX compliant
+
 set -e
 
 # TODO: bootstrap install ansible
@@ -6,6 +8,31 @@ if ! [ -x "$(command -v ansible)" ]; then
 	echo "ansible must be installed"
 	exit 1
 fi
+
+# TODO: bootstrap install openssh
+if ! [ -x "$(command -v ssh-keygen)" ]; then
+	echo "ssh-keygen must be installed"
+	exit 1
+fi
+
+SSH_DIR="$HOME/.ssh"
+SSH_KEY="$SSH_DIR/id_ed25519"
+
+if ! [[ -f "${SSH_KEY}" ]]; then
+	mkdir -p "${SSH_DIR}"
+	chmod 700 "${SSH_DIR}"
+
+	ssh-keygen -b 4096 -t ed25519 -f "${SSH_KEY}" -N "" -C "$USER@$HOSTNAME"
+	chmod 600 "${SSH_KEY}"
+	chmod 600 "${SSH_KEY}.pub"
+
+	cat "${SSH_KEY}" >> "$SSH_DIR/authorized_keys"
+	chmod 600 "$SSH_DIR/authorized_keys"
+
+	eval "$(ssh-agent -s)"
+	ssh-add "${SSH_KEY}"
+fi
+
 
 KALE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$KALE_DIR"
