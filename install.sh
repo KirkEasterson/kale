0625e149f7633fb423388c14064925f8610217f8
Kirk Easterson <kirk.easterson@gmail.com>
2024-08-20 07:36:05 +0200

    feat(install): add prompt for package upgrades

--- install.sh
+++ install.sh
@@ -8,7 +8,16 @@ if ! [ -x "$(command -v ansible)" ]; then
 	exit 1
 fi
 
-KALE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+read -r -p "Do you want to upgrade packages first? (y/N) " yn
+if [[ "$yn" == "y" ]]; then
+	if [ -x "$(command -v yay)" ]; then
+		yay -Syyu
+	else
+		sudo pacman -Syyu
+	fi
+fi
+
+KALE_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd "$KALE_DIR"
 
 if [[ -f "$KALE_DIR/requirements.yml" ]]; then
@@ -16,7 +25,15 @@ if [[ -f "$KALE_DIR/requirements.yml" ]]; then
 fi
 
 if [[ -f "$KALE_DIR/vault-password.txt" ]]; then
-	ansible-playbook --diff --ask-become-pass --vault-password-file "$KALE_DIR/vault-password.txt" "$KALE_DIR/bootstrap.yml"
+	ansible-playbook \
+		--diff \
+		--ask-become-pass \
+		--vault-password-file "$KALE_DIR/vault-password.txt" \
+		"$KALE_DIR/bootstrap.yml"
 else
-	ansible-playbook --diff --ask-become-pass --ask-vault-pass "$KALE_DIR/bootstrap.yml"
+	ansible-playbook \
+		--diff \
+		--ask-become-pass \
+		--ask-vault-pass \
+		"$KALE_DIR/bootstrap.yml"
 fi
